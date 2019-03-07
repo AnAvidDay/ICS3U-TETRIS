bb07dd36d74f8aa5bc82cb596ae44da2fb87702d
David Xiao <davidx@Huogens-MacBook-Pro.local>
2019-03-07 08:17:53 -0500

    added overlay canvas for movement

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,20 @@
 /* retrive grid by saving canvas in a variable */
-const GRID = document.getElementById("main-grid");
+const GRID = document.getElementById("static-grid");
 const CTX = GRID.getContext("2d");
-CTX.fillStyle = "#FF0000";  // color to use for each square
-
-/*
-CTX.fillStyle = "#FF0000";
-CTX.fillRect(35, 0, 35, 35);
-*/
 
 /* constants */
 const SQUARE_PXL = 35;  // num pixels per square on grid
 const HEIGHT = 700;     // height of grid in num pixels
 const WIDTH = 350;      // width of grid in num pixels
 
-/* fill each individual square in the grid */
+/* fill in each individual square in the grid */
 function fill() {
-  for (let i = 0; i <= WIDTH; i += SQUARE_PXL + 1) {
-    for (let j = 0; j <= HEIGHT; j += SQUARE_PXL + 1) {
-      CTX.fillRect(i, j, SQUARE_PXL, SQUARE_PXL);
+  for (let w = 0; w <= WIDTH; w += SQUARE_PXL + 1) {
+    for (let h = 0; h <= HEIGHT; h += SQUARE_PXL + 1) {
+      if (w % 5 == 0) CTX.fillStyle = "#FF0000";  // test
+      else CTX.fillStyle = "blue";
+
+      CTX.fillRect(w, h, SQUARE_PXL, SQUARE_PXL);
     }
   }
 }
