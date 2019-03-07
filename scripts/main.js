a21963328ffa69a0a237bb597ac89f2973cdebcd
David Xiao <davidx@Huogens-MacBook-Pro.local>
2019-03-07 09:59:51 -0500

    set up for tetrominoes

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,23 @@
-/* retrive grid by saving canvas in a variable */
-const GRID = document.getElementById("static-grid");
-const CTX = GRID.getContext("2d");
+/* retrive STATIC_GRID by saving canvas in a variable */
+const STATIC_GRID = document.getElementById("static-grid");
+const STATIC_CTX = STATIC_GRID.getContext("2d");
+const DYNAMIC_GRID = document.getElementById("dynamic-grid");
+const DYNAMIC_CTX = DYNAMIC_GRID.getContext("2d");
 
 /* constants */
-const SQUARE_PXL = 35;  // num pixels per square on grid
-const HEIGHT = 700;     // height of grid in num pixels
-const WIDTH = 350;      // width of grid in num pixels
+const SQUARE_PXL = 35;  // num pixels per square on STATIC_GRID
+const HEIGHT = 700;     // height of STATIC_GRID in num pixels
+const WIDTH = 350;      // width of STATIC_GRID in num pixels
 
-/* fill in each individual square in the grid */
+/* fill in each individual square in the STATIC_GRID */
 function fill() {
   for (let w = 0; w <= WIDTH; w += SQUARE_PXL + 1) {
     for (let h = 0; h <= HEIGHT; h += SQUARE_PXL + 1) {
-      if (w % 5 == 0) CTX.fillStyle = "#FF0000";  // test
-      else CTX.fillStyle = "blue";
+      STATIC_CTX.fillStyle = "grey";
 
-      CTX.fillRect(w, h, SQUARE_PXL, SQUARE_PXL);
+      STATIC_CTX.fillRect(w, h, SQUARE_PXL, SQUARE_PXL);
     }
   }
 }
 
-fill();
+//fill();
