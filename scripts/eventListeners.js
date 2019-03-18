fc08d3be1fb6edf9ce95d4b48fda8a26a1fb77fa
David Xiao <davidx@Huogens-MacBook-Pro.local>
2019-03-18 10:25:19 -0400

    attempt to make movement more responsive

--- scripts/eventListeners.js
+++ scripts/eventListeners.js
@@ -20,8 +20,10 @@ function wall(side) {
 /* add event listeners for key presses */
 document.addEventListener("keydown", function(event) {
   if (event.keyCode == 37 && !wall(1)) {  // move leftward as long as no wall
+    speedLimit = 45;
     col_state -= SQUARE_PXL + 1;
   } else if (event.keyCode == 39 && !wall(2)) { // move rightward as long as no wall
+    speedLimit = 45;
     col_state += SQUARE_PXL + 1;
   } else if (event.keyCode == 40) { // speeds up when holding down
     speedLimit = 5;
