b64ceac7117618423c75d2f61301e77c922f57b6
David Xiao <davidx@Huogens-MacBook-Pro.local>
2019-04-04 09:35:14 -0400

    allows you to set the tetrominoes

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,19 @@ const SQUARE_PXL = 35;  // num pixels per square on STATIC_GRID
 const HEIGHT = 700;     // height of STATIC_GRID in num pixels
 const WIDTH = 350;      // width of STATIC_GRID in num pixels
 
+/* initialize 2d boolean array to tell whether a block
+   is occupied or not */
+var occupied = [];
+var row = []; // temporary row
+for (let i = 1; i <= 20; i++) {
+  row = [];
+  for (let j = 1; j <= 10; j++) {
+    row.push(false);
+  }
+  // push row into occupied
+  occupied.push(row);
+}
+
 /* initialize root position of individual falling tetromino */
 var row_state = -36, col_state = 180;
 
@@ -23,9 +36,17 @@ var currTet = Math.floor(Math.random() * 7); // initial tetromino
 
 /* update canvas by drawing in new position of tetromino */
 function update() {
+  DYNAMIC_CTX.fillStyle = "#bc4d9b";
   for (let i = 0; i < tetr[currTet].length; i++) {
-    DYNAMIC_CTX.fillStyle = "#bc4d9b";
     DYNAMIC_CTX.fillRect(col_state+(tetr[currTet][i][1]*36), row_state+(tetr[currTet][i][0]*36), SQUARE_PXL, SQUARE_PXL);
+  }
+  // draw any set tetrominoes
+  for (let i = 0; i < 20; i++) {
+    for (let j = 0; j < 10; j++) {
+      if (occupied[i][j]) {
+        DYNAMIC_CTX.fillRect(j*36, i*36, SQUARE_PXL, SQUARE_PXL);
+      }
+    }
   }
 }
 
@@ -35,11 +56,27 @@ function check() {
   for (let i = 0; i < tetr[currTet].length; i++) {
     /* current pos for each block*/
     let row = row_state + tetr[currTet][i][0]*36;
-
+    let col = col_state + tetr[currTet][i][1]*36;
     /* see if any obstructions directly below */
-    if (row > HEIGHT - SQUARE_PXL) return true;
+    let in_range = row_state > 0; // cannot check occupied if row_state < 0
+    if (row > HEIGHT - SQUARE_PXL || (in_range && occupied[row/36 + 1][col/36])) {
+      return true;
+    }
   }
   return false;
+}
+
+function add() {
+  /* iterate through every block in the tetromino */
+  for (let i = 0; i < tetr[currTet].length; i++) {
+    /* current pos for each block*/
+    let row = row_state + tetr[currTet][i][0]*36;
+    let col = col_state + tetr[currTet][i][1]*36;
+
+    /* set it in occupied */
+    occupied[row/36][col/36] = true;
+  }
+  console.log(occupied);
 }
 
 /* game loop */
@@ -61,6 +98,7 @@ function loop() {
   if (keyState[40]) {
     speedLimit = 5;
   }
+
   /* draw tetromino */
   /* clear canvas to erase old tetromino */
   DYNAMIC_CTX.clearRect(0, 0, WIDTH+20, HEIGHT+20);
@@ -68,6 +106,8 @@ function loop() {
 
   /* check if tetromino is out of bounds or hits a structure */
   if (check()) {
+    // add the current tetromino to the occupied grid and set it.
+    add();
     currTet = Math.floor(Math.random() * 7); // generate new tetromino
     row_state = -36; col_state = 180;        // reset to initial pos
   }
