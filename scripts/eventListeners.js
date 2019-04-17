b34e39f9c52a741cbc7093a801253db3ee2faea2
David Xiao <davidx@Huogens-MacBook-Pro.local>
2019-04-17 13:48:30 -0400

    You can't move into blocks anymore

--- scripts/eventListeners.js
+++ scripts/eventListeners.js
@@ -5,12 +5,22 @@ function wall(side) {
   /* iterate through every block in the tetromino */
   for (let i = 0; i < tetr[currTet].length; i++) {
     /* current pos for each block*/
-    let col = col_state + tetr[currTet][i][1]*36;
+    let col = col_state + tetr[currTet][i][1] * 36;
+    let row = row_state + tetr[currTet][i][0] * 36;
 
-    /* see if obstructions on left or right side */
+    // to make sure tetrominoe does not go out-of-bounds
     if (col <= 0 && side == 1) {
       return true;
     } else if (col >= WIDTH - SQUARE_PXL && side == 2) {
+      return true;
+    }
+
+    /* see if obstructions on left or right side
+      by checking for tetriminoes
+    */
+    if (occupied[Math.max(0, row/36)][col/36 - 1] && 1) {
+      return true;
+    } else if (occupied[Math.max(0, row/36)][col/36 + 1] && 2) {
       return true;
     }
   }
